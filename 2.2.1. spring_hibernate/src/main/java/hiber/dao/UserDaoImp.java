5c22f698095b5767b6f0ff99d9c1a2e689e2387e
andro <androidmaster318@gmail.com>
2024-01-15 23:09:40 +0300

    Add Car

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,6 +1,7 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -26,4 +27,21 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+
+
+
+
+    @Override
+    public User getUserByCar(String model, int series) {
+
+         String HQL="FROM User where car.model =:model and car.series = :series";
+         Query<User> query = sessionFactory.getCurrentSession().createQuery(HQL, User.class);
+         query.setParameter("model", model);
+         query.setParameter("series", series);
+
+         return query.getSingleResult();
+   }
+
 }
+
+
